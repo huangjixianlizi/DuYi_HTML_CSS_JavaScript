e2e54df4bcd4fb0b602ede8349952bb7b0c3aa62
huangjixianlizi <huangjixianlizi@126.com>
2018-10-31 16:55:04 +0800

    m

--- myJavaScript.js
+++ myJavaScript.js
@@ -12,27 +12,27 @@
 //     	startMove(oDivArray[1], targetObj);
 //     });
 // }
-function startMove(obj, json, callback) {
+function startMove(obj, data, callback) {
     clearInterval(obj.timer);
     var timer = null;
     var iSpeed,
         iCur;
     obj.timer = setInterval(function() {
         var bStop = true;
-        for (var attr in json) {
+        for (var attr in data) {
             if (attr == 'opacity') {
                 iCur = parseFloat(getStyle(obj, attr)) * 100;
             } else {
                 iCur = parseInt(getStyle(obj, attr));
             }
-            iSpeed = (json[attr] - iCur) / 7;
+            iSpeed = (data[attr] - iCur) / 7;
             iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
             if (attr == 'opacity') {
                 obj.style.opacity = (iSpeed + iCur) / 100;
             } else {
                 obj.style[attr] = iSpeed + iCur + 'px';
             }
-            if (iCur != json[attr]) {
+            if (iCur != data[attr]) {
                 bStop = false;
             }
         }
@@ -42,6 +42,41 @@ function startMove(obj, json, callback) {
         }
     }, 30)
 }
+//版本二
+// function startMove(obj, data, func) {
+//     clearInterval(obj.timer);
+//     var timer = null;
+//     var iSpeed,
+//         iCur,
+//         name;
+//     obj.timer = setInterval(function() {
+//         var bStop = true;
+//         for (var attr in data) {
+//             if (attr === 'opacity') {
+//                 iCur = parseFloat(getStyle(obj, attr)) * 100;
+//             } else {
+//                 iCur = parseInt(getStyle(obj, attr));
+//             }
+//             iSpeed = (data[attr] - iCur) / 8;
+//             iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
+//             if (attr === 'opacity') {
+//                 obj.style.opacity = (iSpeed + iCur) / 100;
+//             } else {
+//                 obj.style[attr] = iSpeed + iCur + 'px';
+//             }
+//             if (Math.floor(Math.abs(data[attr] - iCur)) != 0 ) {
+//                 bStop = false;
+//             }
+//         }
+//         if (bStop) {
+//             clearInterval(obj.timer);
+//             if(name === 'opacity'){
+//                 obj.style.opacity = data[name] / 100;
+//             }
+//             func();
+//         }
+//     }, 30)
+// }
 
 
 //数组 ： 封装一个myReduce方法
